1ba6aa5e23c16d6935c3bbc9c705a83e920efc52
vishal563 <vishal563@gmail..com>
2020-04-10 11:43:20 +0530

    Updates

---
+++ precache-manifest.9a2a62c3da9c275f03612f31e45573ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea335cfb210c1ea9d0466a5e5d7d53b",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "31f3d2644c1a18b42317",
+    "url": "/deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "724d4a02990617c1e8f5",
+    "url": "/deploy/static/js/2.9b69bad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.9b69bad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f3d2644c1a18b42317",
+    "url": "/deploy/static/js/main.366161ab.chunk.js"
+  },
+  {
+    "revision": "c6f45327451a697a39ec",
+    "url": "/deploy/static/js/runtime-main.33ab80cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
